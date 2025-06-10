49c5c964ed112976e64b38406e03d4a661c57445
dajojoap <dajojoap@unal.edu.co>
2025-06-09 23:11:08 -0500

    Test7, imagenes

--- import pygame.py
+++ import pygame.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 #abrir la ventana (pop up) 
 pygame.init()
@@ -6,25 +7,34 @@ Ventana_Altura, Ventana_Ancho =824, 527 #esto define cuanto mide la pantalla
 Display_Surface= pygame.display.set_mode((Ventana_Ancho, Ventana_Altura)) #esto saca la pantalla
 corriendo= True #esto hace que la pantalla corra
 
-#surface
+#plain surface
 surf= pygame.Surface((100,200))
 surf.fill((3,115,17))
 x = 100
 
+#importando una imagen
+player_Surf= pygame.image.load("Assets\\Buho test.png")
+Moneda_surf= pygame.image.load("Assets\\Moneda_test.png")
+Moneda_positions = [(randint(0, Ventana_Ancho), randint(0, Ventana_Altura))for i in range(20)]
+
 #titulo e icono
 pygame.display.set_caption('Escapa de la UN') #define el titulo de la ventana
-Icono= pygame.image.load("C:\\Users\\user\\Desktop\\GAME TEST\\ASSETS\\Icon.jpg") #esto nos da el icono de la pantalla (cambiarlo por la direccion de la imagen a usar)
+Icono= pygame.image.load("Assets\\Icon128.jpg")
 pygame.display.set_icon(Icono)
 
-while True:
+while corriendo:
     #loop de evento
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             corriendo= False
 
-     
     #dibuja el juego
-    Display_Surface.fill((4,17,4)) #esto define el color de fondo de la pantalla
+    Display_Surface.fill((4,17,4)) 
+    for pos in Moneda_positions:
+        Display_Surface.blit(Moneda_surf,pos)
     x += 0.1
-    Display_Surface.blit(surf, (x,150))
+    Display_Surface.blit(player_Surf, (x,150))
+
     pygame.display.update()
+
+pygame.quit()
